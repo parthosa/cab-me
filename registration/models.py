e778a78084ba508d97e3f3c3a89c3896f1f1792a
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-05 22:37:18 +0530

    Made models for registeration

---
+++ registration/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from allauth.socialaccount.models import *
+from cab.models import *
+
+
+class UserProfile(models.Model):
+    name = models.CharField('First Name', max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=100)
+    phone = models.BigIntegerField()
+    email_id = models.EmailField(unique=True)
+    user = models.OneToOneField(User, SocialAccount, null=True)
+    postedcabs = models.ManyToManyField(PostCab, blank = True, default = '')
+    bookedcabs = models.ManyToManyField(BookCab, blank = True, default = '')
+    def __unicode__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "User Profiles"
