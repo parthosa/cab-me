a865a314da3635f7d55e3420d54dedf17dd477c4
ashu <jainamritanshu@gmail.com>
2016-11-10 01:17:41 +0530

    sms api working and booking cab

--- cab/views.py
+++ cab/views.py
@@ -4,6 +4,7 @@ from registration.models import *
 from django.http import HttpResponseRedirect,Http404,HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+import requests
 
 
 def index(request):
@@ -15,7 +16,7 @@ def index(request):
 		return render(request, 'cab/index.html', context)
 	else:
 		user = request.user
-		print user
+		print user.username
 		customer = UserProfile.objects.get(email_id = user.username)
 		name = customer.name
 		context = {'name': name}
@@ -43,6 +44,7 @@ def bookcab(request):
 		b_cab.From = request.POST['From']
 		b_cab.To = request.POST['To']
 		b_cab.Date = request.POST['Date']
+		b_cab.Date_return = request.POST['Date_return']
 		b_cab.Time = request.POST['Time']
 		b_cab.OneWay = request.POST['OneWay']
 		b_cab.Sharing = request.POST['Sharing']
@@ -66,6 +68,14 @@ def bookcab(request):
 		type_cab = []
 		cab_id = []
 		cust_names = []
+		From = []
+		To = []
+		Date = []
+		Date_return = []
+		Time = []
+		OneWay = []
+		Sharing = []
+
 		if b_cab.OneWay == True and b_cab.Sharing == True:
 			for cab in p_cabs:
 				D_pcab = p_cabs.user
@@ -78,6 +88,14 @@ def bookcab(request):
 				cab_id.append(p_cab.cab_id) # not for display to users only for returning in the post request to backend
 				# pcab_id = p_cabs.id # not for display to users only for returning in the post request to backend
 				cust_names.append(D_pcabuserp.name)
+				From.append(b_cab.From)
+				To.append(b_cab.To)
+				Date.append(b_cab.Date)
+				Date_return.append(b_cab.Date_return)
+				Time.append(b_cab.Time)
+				OneWay.append(b_cab.OneWay)
+				Sharing.append(b_cab.Sharing)
+
 			for cab in share_cabs:
 
 				D_name.append(cab.DriverName)
@@ -87,6 +105,14 @@ def bookcab(request):
 				cab_id.append(cab.cab_id) # not for display to users only for returning in the post request to backend
 				# pcab_id = p_cab.id # not for display to users only for returning in the post request to backend
 				cust_names = [customer.name for customer in cab.Cust]
+				From.append(b_cab.From)
+				To.append(b_cab.To)
+				Date.append(b_cab.Date)
+				Date_return.append(b_cab.Date_return)
+				Time.append(b_cab.Time)
+				OneWay.append(b_cab.OneWay)
+				Sharing.append(b_cab.Sharing)
+
 			for cab in cabs:
 
 				D_name.append(cab.DriverName)
@@ -96,8 +122,15 @@ def bookcab(request):
 				cab_id.append(cab.cab_id) # not for display to users only for returning in the post request to backend
 				# pcab_id = p_cab.id # not for display to users only for returning in the post request to backend
 				cust_names.append('none')
+				From.append(b_cab.From)
+				To.append(b_cab.To)
+				Date.append(b_cab.Date)
+				Date_return.append(b_cab.Date_return)
+				Time.append(b_cab.Time)
+				OneWay.append(b_cab.OneWay)
+				Sharing.append(b_cab.Sharing)
 
-			resp = {'Driver_name': D_name, 'Price': Price, 'Cab_type': type_cab, 'cab_id': cab_id, 'cust_names': cust_names}
+			resp = {'Driver_name': D_name, 'Price': Price, 'Cab_type': type_cab, 'cab_id': cab_id, 'cust_names': cust_names ,'From': From, 'To': To, 'Date': Date, 'Date_return': Date_return,'Time': Time, 'OneWay': OneWay, 'Sharing': Sharing}
 
 		elif b_cab.OneWay == True and b_cab.Sharing == False:
 			D_name.append(cab.DriverName)
@@ -105,8 +138,14 @@ def bookcab(request):
 			# price_pcab = p_cab.price
 			type_cab.append(cab.Type) 
 			cab_id.append(cab.cab_id) # not for display to users only for returning in the post request to backend
-
-			resp = {'Driver_name': D_name, 'Price': Price, 'Cab_type': type_cab, 'cab_id': cab_id}
+			From.append(b_cab.From)
+			To.append(b_cab.To)
+			Date.append(b_cab.Date)
+			Date_return.append(b_cab.Date_return)
+			Time.append(b_cab.Time)
+			OneWay.append(b_cab.OneWay)
+			Sharing.append(b_cab.Sharing)
+			resp = {'Driver_name': D_name, 'Price': Price, 'Cab_type': type_cab, 'cab_id': cab_id,'From': From, 'To': To, 'Date': Date, 'Date_return': Date_return,'Time': Time, 'OneWay': OneWay, 'Sharing': Sharing}
 		
 		else:
 			days = request.POST['Days']
@@ -116,30 +155,96 @@ def bookcab(request):
 			type_cab.append(cab.Type) 
 			cab_id.append(cab.cab_id) # not for display to users only for returning in the post request to backend
 			
-			resp = {'Driver_name': D_name, 'Price': Price, 'Cab_type': type_cab, 'cab_id': cab_id}
+			From.append(b_cab.From)
+			To.append(b_cab.To)
+			Date.append(b_cab.Date)
+			Date_return.append(b_cab.Date_return)
+			Time.append(b_cab.Time)
+			OneWay.append(b_cab.OneWay)
+			Sharing.append(b_cab.Sharing)
+
+			resp = {'Driver_name': D_name, 'Price': Price, 'Cab_type': type_cab, 'cab_id': cab_id,'From': From, 'To': To, 'Date': Date, 'Date_return': Date_return,'Time': Time, 'OneWay': OneWay, 'Sharing': Sharing}
 		return JsonResponse(resp)
 
 @login_required
 @csrf_exempt
 def booknow(request, user):
-	user_p = UserProfile.objects.get(user = request.user)
-	#if book now in request.POST, partho will return id of the cab booked to the backend
-	cab_id = request.POST['cab_id']
-	# pcab_id = request.POST['pcab_id']
-	if not cab_id.startswith('p'): #(pcab_id == null):
-	#try:	
-		cab_b = Cab.objects.get(pk = cab_id)
-	#except ObjectDoesNotExist:
-	else:
-		cab_b = PostCab.objects.get(pk = cab_id)
+	if request.POST:
+		user_p = UserProfile.objects.get(user = request.user)
+		#if book now in request.POST, partho will return id of the cab booked to the backend
+		cab_id = request.POST['cab_id']
+		# pcab_id = request.POST['pcab_id']
 
-	user_p.bookedcabs.add(cab_b) 
-	#request.session['feedback'] = cab_b
-	key = request.user.id
-	cache.set(key,
-		{'booked': True})
+		if not cab_id.startswith('p'): #(pcab_id == null):
+		#try:	
+			cab_b = Cab.objects.get(pk = cab_id)
 
-	resp = {'status': 'success', 'message': 'Your cab has been booked'}
+			sms_body_cust = '''Hi %s,
+		Your Cab will be confirmed with in 15 mins from %s to %s by CabMe.
+		Driver/Owner name: Reddy Kumar Simha
+		Driver/Owner number: 8890605392
+		Cab Type: %s
+		''' % (user_p.name, From, To, cab_b.Type)
+			requests.get('http://bhashsms.com/api/sendmsg.php?user=8890605392&pass=narasimha132&sender=CabMee&phone=%s&text=%s&priority=dnd&stype=normal') % (user_p.phone, sms_body_cust)
+			
+			sms_body_simha = '''%s has requested a cab from %s to %s.
+		Customer name: %s
+		Customer number: %s
+		Cab Type: %s
+		''' % (user_p.name, From, To, user_p.name, user_p.phone, cab_b.Type)
+			requests.get('http://bhashsms.com/api/sendmsg.php?user=8890605392&pass=narasimha132&sender=CabMee&phone=8890605392&text=%s&priority=dnd&stype=normal') % (sms_body_simha)
+			b_cab = BookCab()
+			b_cab.From = request.POST['From']
+			b_cab.To = request.POST['To']
+			b_cab.Date = request.POST['Date']
+			b_cab.Date_return = request.POST['Date_return']
+			b_cab.Time = request.POST['Time']
+			b_cab.OneWay = request.POST['OneWay']
+			b_cab.Sharing = request.POST['Sharing']
+
+			b_cab.save()
+
+		else:
+			cab_b = PostCab.objects.get(pk = cab_id)
+			user_driver = cab_b.user
+			userpro_driver = UserProfile.objects.get(user = user_driver)
+			sms_body_cust = '''Hi %s,
+		Your Cab will be confirmed by %s with in 15 mins from %s to %s.
+		Driver/Owner name: %s
+		Driver/Owner number: %s
+		Cab Type: %s
+		''' % (user_p.name,userpro_driver.name, From, To, userpro_driver.name, userpro_driver.phone, cab_b.Type)
+			requests.get('http://bhashsms.com/api/sendmsg.php?user=8890605392&pass=narasimha132&sender=CabMee&phone=%s&text=%s&priority=dnd&stype=normal') % (user_p.phone, sms_body_cust)
+			
+			confirm_url = 'cabme.in'
+				sms_body_driver = '''Hi %s,
+			%s has requested to pool in your car from %s to %s.
+			Kindly confirm his request by following the url %s, or logging in cabme portal.
+			User Contact Number: %s
+			''' % (userpro_driver.name, user_p.name, From, To, confirm_url ,user_p.phone)
+			requests.get('http://bhashsms.com/api/sendmsg.php?user=8890605392&pass=narasimha132&sender=CabMee&phone=%s&text=%s&priority=dnd&stype=normal') % (userpro_driver.phone, sms_body_driver)
+
+		user_p.bookedcabs.add(cab_b) 
+		#request.session['feedback'] = cab_b
+		key = request.user.id
+		# b_cab = BookCab()
+		# b_cab.From = request.POST['From']
+		# b_cab.To = request.POST['To']
+		# b_cab.Date = request.POST['Date']
+		# b_cab.Date_return = request.POST['Date_return']
+		# b_cab.Time = request.POST['Time']
+		# b_cab.OneWay = request.POST['OneWay']
+		# b_cab.Sharing = request.POST['Sharing']
+		cache.set(key,
+			{'booked': True})
+		# sms_body = '''Hi %s,
+		# Your Cab will be confirmed with in 15 mins from %s to %s by CabMe.
+		# Driver/Owner name: Reddy Kumar Simha
+		# Driver/Owner number: 8890605392
+		# Cab Type: %s
+		# ''' % (user_p.name, From, To, cab_b.Type)
+		# requests.get('http://bhashsms.com/api/sendmsg.php?user=8890605392&pass=narasimha132&sender=CabMee&phone=%s&text=%s&priority=dnd&stype=normal') % (user_p.phone, sms_body)
+		resp = {'status': 'success', 'message': 'Your cab has been booked'}
 
 @login_required
 @csrf_exempt
