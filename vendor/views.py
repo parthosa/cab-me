f652d4c619afc004a46226277878f264ce22dc88
Partho Sarthi <psarthi16@gmail.com>
2017-01-04 02:22:19 +0530

    moments err

--- vendor/views.py
+++ vendor/views.py
@@ -3,7 +3,7 @@ from .models import *
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
-import moment
+# import moment
 from datetime import datetime
 from django.core.cache import cache
 from django.contrib.auth import authenticate, login
