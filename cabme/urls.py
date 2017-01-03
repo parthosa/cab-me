a616b8fab7a9b2eba78ba96d142146a075a4376b
ashu <jainamritanshu@gmail.com>
2017-01-03 19:15:52 +0530

    allauth and url changes

--- cabme/urls.py
+++ cabme/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -9,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.urls', namespace='registration')),
     url(r'^', include('cab.urls', namespace='cab')),
-    url(r'^accounts/social/', include('allauth.urls'))
+    url(r'^accounts/social/', include('allauth.urls')),
+     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.STATIC_ROOT, })
 )
